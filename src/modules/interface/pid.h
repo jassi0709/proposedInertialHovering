8924273782984173f2fe64cc6948feec488dc1b2
jaskirat.iisPC.mint <jsingh@student.ethz.ch>
2016-11-29 11:45:53 +0100

    Added the personal PID parameters.

--- src/modules/interface/pid.h
+++ src/modules/interface/pid.h
@@ -32,35 +32,36 @@
 
 #if defined(PLATFORM_CF2)
 
-#define PID_ROLL_RATE_KP  250.0
-#define PID_ROLL_RATE_KI  500.0
-#define PID_ROLL_RATE_KD  2.5
-#define PID_ROLL_RATE_INTEGRATION_LIMIT    33.3
+#define PID_ROLL_RATE_KP  100.0						//250.0
+#define PID_ROLL_RATE_KI  250.0						//500.0
+#define PID_ROLL_RATE_KD  2.0						//2.5
+#define PID_ROLL_RATE_INTEGRATION_LIMIT    33.3		//33.3
 
-#define PID_PITCH_RATE_KP  250.0
-#define PID_PITCH_RATE_KI  500.0
-#define PID_PITCH_RATE_KD  2.5
-#define PID_PITCH_RATE_INTEGRATION_LIMIT   33.3
+#define PID_PITCH_RATE_KP  70.0						//250.0
+#define PID_PITCH_RATE_KI  170.0					//500.0
+#define PID_PITCH_RATE_KD  0.0						//2.5
+#define PID_PITCH_RATE_INTEGRATION_LIMIT   33.3		//33.3
 
-#define PID_YAW_RATE_KP  120.0
-#define PID_YAW_RATE_KI  16.7
-#define PID_YAW_RATE_KD  0.0
-#define PID_YAW_RATE_INTEGRATION_LIMIT     166.7
+#define PID_YAW_RATE_KP  120.0						//120
+#define PID_YAW_RATE_KI  16.7						//16.7
+#define PID_YAW_RATE_KD  0.0						//0.0
+#define PID_YAW_RATE_INTEGRATION_LIMIT     166.7	//166.7
 
-#define PID_ROLL_KP  6.0
-#define PID_ROLL_KI  3.0
-#define PID_ROLL_KD  0.0
-#define PID_ROLL_INTEGRATION_LIMIT    20.0
+//these are the attitude PID parameters
+#define PID_ROLL_KP  3.4						//6.0
+#define PID_ROLL_KI  2.0						//3.0
+#define PID_ROLL_KD  0.0						//0.0
+#define PID_ROLL_INTEGRATION_LIMIT    20.0		//20.0
 
-#define PID_PITCH_KP  6.0
-#define PID_PITCH_KI  3.0
-#define PID_PITCH_KD  0.0
-#define PID_PITCH_INTEGRATION_LIMIT   20.0
+#define PID_PITCH_KP  3.2						//6.0
+#define PID_PITCH_KI  2.0						//3.0
+#define PID_PITCH_KD  0.0						//0.0
+#define PID_PITCH_INTEGRATION_LIMIT   20.0		//20.0
 
-#define PID_YAW_KP  6.0
-#define PID_YAW_KI  1.0
-#define PID_YAW_KD  0.35
-#define PID_YAW_INTEGRATION_LIMIT     360.0
+#define PID_YAW_KP  1.0							//6.0
+#define PID_YAW_KI  0.0							//1.0
+#define PID_YAW_KD  0.00						//0.35
+#define PID_YAW_INTEGRATION_LIMIT     10.0		//350
 
 
 #define DEFAULT_PID_INTEGRATION_LIMIT  5000.0
